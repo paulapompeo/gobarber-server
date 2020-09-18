425978d60a75cb5855632d07bce15c422f10e38f
Paula Pompeo de Camargo Ranazzi <paulapompeo@ibm.com>
2020-09-17 23:04:16 -0300

    testa combinacao de senha e existencia de usuario

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -26,4 +26,37 @@ describe('AuthenticateUser', () => {
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
   });
+
+  it('should not be able to authenticate with non existing user', async () => {
+    const fakeUsersRespository = new FakeUsersRespository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(fakeUsersRespository, fakeHashProvider);
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@example.com',
+        password: '1234567',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+  
+  it('should not be able to authenticate with wrong password/email combination', async () => {
+    const fakeUsersRespository = new FakeUsersRespository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(fakeUsersRespository, fakeHashProvider);
+    const authenticateUser = new AuthenticateUserService(fakeUsersRespository, fakeHashProvider);
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '1234567',
+    })
+
+    expect(authenticateUser.execute({
+      email: 'johndoe@example.com',
+      password: 'worng-password'
+    })).rejects.toBeInstanceOf(AppError);
+  });
 });
