abdbf1c59469a030be64095f3c2e9d2fad515050
Paula Pompeo de Camargo Ranazzi <paulapompeo@ibm.com>
2020-09-22 00:45:17 -0300

    Teste para geração de token

--- src/modules/users/services/SendForgotPasswordEmailService.spec.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -1,30 +1,64 @@
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 
-import FakeUsersRespository from '../repositories/fakes/FakeUsersRepository';
 import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+import FakeUsersRespository from '../repositories/fakes/FakeUsersRepository';
+import FakeUserTokensRespository from '../repositories/fakes/FakeUserTokensRepository';
 import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
 
+let fakeUsersRespository: FakeUsersRespository;
+let fakeMailProvider: FakeMailProvider;
+let fakeUserTokensRespository: FakeUserTokensRespository;
+let sendForgotPasswordEmail: SendForgotPasswordEmailService;
+
 describe('SendForgotPasswordEmail', () => {
+  beforeEach(() => {
+    fakeUsersRespository = new FakeUsersRespository();
+    fakeMailProvider = new FakeMailProvider();
+    fakeUserTokensRespository = new FakeUserTokensRespository();
+
+    sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+      fakeUsersRespository,
+      fakeMailProvider,
+      fakeUserTokensRespository
+    );
+  })
+
   it('should be able to recorver password using email', async () => {
-    const fakeUsersRespository = new FakeUsersRespository();
-    const fakeMailProvider = new FakeMailProvider();
-
+   
     const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
-
-    const SendForgotPasswordEmail = new SendForgotPasswordEmailService(fakeUsersRespository, fakeMailProvider);
-
     await fakeUsersRespository.create({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: '123456',
     });
 
-    await SendForgotPasswordEmail.execute({
+    await sendForgotPasswordEmail.execute({
       email: 'johndoe@example.com',
     });
 
     expect(sendMail).toHaveBeenCalled();
   });
+
+  it('should not be able to recover a non-existing user password', async () => {
+    await expect(sendForgotPasswordEmail.execute({
+      email: 'johndoe@example.com',
+    })).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should generate a forgot password token', async () => {
+    const generateToken = jest.spyOn(fakeUserTokensRespository, 'generate');
+    const user = await fakeUsersRespository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    await sendForgotPasswordEmail.execute({
+      email: 'johndoe@example.com',
+    });
+
+    expect(generateToken).toHaveBeenCalledWith(user.id);
+  })
 });
 
 // RED
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,13 +3,26 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
-// import IMailProvider from './StorageProvider/models/.r';
-// import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
+import IUsersRepository from '@modules/users/repositories/IUserTokensRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+
+// import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+// import UserTokensRepository from '@modules/users/infra/typeorm/UserTokensRepository';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
+);
+
+// container.registerSingleton<IStorageProvider>(
+//   'StorageProvider',
+//   DiskStorageProvider,
+// );
 
 
 
