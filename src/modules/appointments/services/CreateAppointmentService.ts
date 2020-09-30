0731469c92924f68a7f781967137638a16d1137c
Paula Pompeo de Camargo Ranazzi <paulapompeo@ibm.com>
2020-09-29 21:09:45 -0300

    envia notificações de agendamento

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,10 +1,11 @@
-import { startOfHour, isBefore, getHours } from 'date-fns';
+import { startOfHour, isBefore, getHours, format } from 'date-fns';
 import { injectable, inject } from 'tsyringe'; 
 
 import AppError from '@shared/errors/AppError';
 
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
 
 interface IRequest {
   provider_id: string;
@@ -17,6 +18,9 @@ class CreateAppointmentService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+    
+    @inject('NotificationsRepository')
+    private notificationsRepository: INotificationsRepository,
     ) {}
 
   public async execute({ provider_id,user_id, date }: IRequest): Promise<Appointment> {
@@ -47,6 +51,15 @@ class CreateAppointmentService {
       user_id,
       date: appointmentDate,
     });
+
+    //data formatada
+    const dateFormated = format(appointmentDate, "dd/MM/yyyy 'às' HH'h'mm");
+
+    await this.notificationsRepository.create({
+      recipient_id: provider_id,
+      content: `Novo agendamento para dia ${dateFormated}`,
+    });
+    
     return appointment;
   }
 }
