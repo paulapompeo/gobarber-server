8ee2005e7d8a3b86883e4994a99cf712292b865d
Paula Pompeo de Camargo Ranazzi <paulapompeo@ibm.com>
2020-10-23 17:08:45 -0300

    lista providers com serializacao

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -4,6 +4,7 @@ import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICa
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 import User from '@modules/users/infra/typeorm/entities/User';
+import { classToClass } from 'class-transformer';
 
 interface IRequest {
   user_id: string;
@@ -22,12 +23,14 @@ class ListProvidersService {
   public async execute({user_id}: IRequest): Promise<User[]> {
     let users = await this.cacheProvider.recover<User[]>(`providers-list:${user_id}`);
 
+    // let users;
+
     if (!users) {
       users =  await this.usersRepository.findAllProviders({except_user_id: user_id});
 
       // console.log('A query no banco foi realizada!');
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(`providers-list:${user_id}`, classToClass(users));
     }
     
     return users;
